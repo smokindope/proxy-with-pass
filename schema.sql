fff40aa040ab9262a48e4916db57a9e06d33310f
smokindope <smokindopemcfc247@gmail.com>
2026-06-02 22:25:18 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,2 @@
-CREATE TABLE settings (
-  id INTEGER PRIMARY KEY,
-  password TEXT NOT NULL
-);
-
-INSERT INTO settings (id, password)
-VALUES (1, 'change-me-now');
+CREATE TABLE settings ( id INTEGER PRIMARY KEY, password TEXT NOT NULL);
+INSERT INTO settings (id, password) VALUES (1, 'change-me-now');
